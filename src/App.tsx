523bba7c9b73520b8ee5fb30baf6a5e26ed4700c
44437 <ercantopuz33@gmail.com>
2024-09-09 11:58:23 +0300

    Implement the design

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
 const App = () => (
   <>
-    <div className="flex bg-c_blue h-full min-h-screen items-center justify-center">
-      <div className="bg-white flex flex-col w-[320px] p-4 rounded-[20px]">
+    <div className="flex items-center justify-center h-full min-h-screen bg-[#F4D04E]">
+      <div className="flex flex-col font-figtree min-w-[327px] w-[384px] rounded-[20px] bg-white p-[23px] border border-black shadow-[rgba(0,0,0,1)_8px_8px_1px_1px]">
         <img
           className="rounded-[10px]"
-          src="./images/image-qr-code.png"
-          alt="image-qr-code.png"
+          src="/images/illustration-article.svg"
+          alt=""
         />
-        <div className="h-[24px]"></div>
-        <div className=" mx-4">
-          <p className="text-[#1F314F] font-bold text-center	text-[22px] leading-120 font-outfit">
-            Improve your front-end skills by building projects
+        <div className="my-6">
+          <div className="py-1 px-3 rounded-[4px] bg-[#F4D04E] inline-block">
+            <p className="text-[14px] font-extrabold leading-150">Learning</p>
+          </div>
+          <div className="h-3" />
+          <p className="text-[14px] font-medium leading-150">
+            Published 21 Dec 2023
           </p>
-          <div className="h-[16px]"></div>
-          <p className="text-[#68778D] tracking-0.2 font-normal text-center	text-[15px] leading-140 font-outfit">
-            Scan the QR code to visit Frontend Mentor and take your coding
-            skills to the next level
+          <div className="h-3" />
+          <p className="text-[24px] font-extrabold leading-150">
+            HTML & CSS foundations
+          </p>
+          <div className="h-3" />
+          <p className="text-[16px] text-[#6B6B6B] font-medium leading-150">
+            These languages are the backbone of every website, defining
+            structure, content, and presentation.
           </p>
         </div>
-        <div className="h-[24px]"></div>
+        <div className="flex flex-row items-center">
+          <img
+            className="h-8 w-8 rounded-full"
+            src="images/image-avatar.webp"
+            alt=""
+          />
+          <div className="w-3" />
+          <p className="text-[14px] font-extrabold leading-150">Greg Hooper</p>
+        </div>
       </div>
     </div>
   </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,15 +3,14 @@ export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      colors: {
-        c_blue: "#D5E1EF",
+      boxShadow: {
+        c_black_shadow: "0 8px 8px 0 rgba(0, 0, 0, 1)",
       },
       fontFamily: {
-        outfit: ["Outfit", "sans-serif"],
+        figtree: ["Figtree", "sans-serif"],
       },
       lineHeight: {
-        120: "120%",
-        140: "140%",
+        150: "150%",
       },
       letterSpacing: {
         0.2: "0.2px",
